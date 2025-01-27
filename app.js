25313fbd6866386c0ef2b2bd72e13ed6202ca01a
viole0416 <violeta.pino25@gmail.com>
2025-01-27 09:41:48 -0300

    Subiendo parte de la logica

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+  // Obtener el valor del input
+  let campoNombre = document.getElementById("nombreAmigo");
+  let nombreIngresado = input.value.trim(); // El .trim elimina los espacios en blanco que por error esten escritos en el cuadro de texto
+
+  if (nombreIngresado === "") {
+    alert("Por favor inserte un nombre");
+    return;
+  }
+  if (amigos.includes(nombreIngresado)) {
+    alert("Este nombre ya esta en la lista");
+    return;
+  }
+  amigos.push(nombreIngresado);
+  input.value = "";
+}
+
+console.log(amigos);
